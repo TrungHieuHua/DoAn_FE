bd3e720c6ce258a6885965cbf1f0e5cc851cd433
TrungHieu <hieuhua7802@gmail.com>
2025-05-16 20:27:45 +0700

    fix

--- admin/src/ultils/services/AuthService.js
+++ admin/src/ultils/services/AuthService.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const login = async (username, password) => {
     try {
-        const response = await axios.post('http://localhost:8080/api/v1npnp', {
+        const response = await axios.post('http://localhost:8080/api/v1/login', {
             username,
             password,
         });
